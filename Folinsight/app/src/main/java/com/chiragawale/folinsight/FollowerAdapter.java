80afe0b7ba2b1eecd3cc36e9933a5482d06573a8
Chirag Awale <chiragawale777@gmail.com>
2017-07-07 03:49:22 -0500

    Adapter synced with loader which is sycned with list view

---
+++ Folinsight/app/src/main/java/com/chiragawale/folinsight/FollowerAdapter.java
@@ -0,0 +1,50 @@
+package com.chiragawale.folinsight;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.chiragawale.folinsight.entity.Follower;
+
+import java.util.List;
+
+
+
+public class FollowerAdapter extends ArrayAdapter<Follower> {
+    /*
+    Constructor
+     */
+    public FollowerAdapter(Context context, List<Follower> followerList) {
+        super(context, 0,followerList);
+    }
+
+    @Override
+    public View getView(int position,  View view, ViewGroup parent) {
+        // Check if there is an existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View listItemView = view;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        //Localizing the text views that display data to set their text
+        TextView follower_name = (TextView) listItemView.findViewById(R.id.follower_name);
+        TextView follower_userName = (TextView) listItemView.findViewById(R.id.follower_user_name);
+        TextView follower_track_date = (TextView) listItemView.findViewById(R.id.follower_tracked_date);
+
+        //Get the current follower object
+        Follower currentFollower = getItem(position);
+
+        //Setting the texts of their respective textViews according to the data received from the List
+        follower_name.setText(currentFollower.getFullName());
+        follower_userName.setText(currentFollower.getUserName());
+        follower_track_date.setText("Feb 21, 2017");
+
+        return listItemView;
+    }
+}
--- Folinsight/app/src/main/java/com/chiragawale/folinsight/MainActivity.java
+++ Folinsight/app/src/main/java/com/chiragawale/folinsight/MainActivity.java
@@ -4,37 +4,59 @@ import android.app.LoaderManager;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.chiragawale.folinsight.entity.Follower;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Follower>> {
+    /*
+    URL TO CONNECT TO, TO GET FOLLOWER DATA
+     */
     private final String FOLLOWER_DATA_URL = "https://api.instagram.com/v1/users/self/followed-by?access_token=3883966149.9708451.f09f4c2b9a014205a5d4eb91a4841226";
+    //Adapter for providing data to the list view
+    private FollowerAdapter mFollowerAdapter;
 
-    private TextView test;
+    //Loader Id for follower loader
+    private final static int FOLLOWER_LOADER_ID = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test = (TextView) findViewById(R.id.test);
 
-        getLoaderManager().initLoader(0,null,this);
+        //Initializing the adapter with null list
+        mFollowerAdapter = new FollowerAdapter(this, new ArrayList<Follower>());
+
+        //Linking the adapter to the list view
+        ListView listView = (ListView) findViewById(R.id.follower_list);
+        listView.setAdapter(mFollowerAdapter);
+
+        //Kicks off the loader
+        getLoaderManager().initLoader(FOLLOWER_LOADER_ID, null, this);
     }
 
+    //When the loader is created
     @Override
     public Loader<List<Follower>> onCreateLoader(int id, Bundle args) {
-        return new FollowerLoader(this,FOLLOWER_DATA_URL);
+        return new FollowerLoader(this, FOLLOWER_DATA_URL);
     }
 
+    //When the loader is done loading the data
     @Override
     public void onLoadFinished(Loader<List<Follower>> loader, List<Follower> data) {
-    test.setText(data.get(0).getFullName());
+        mFollowerAdapter.clear();
+
+        mFollowerAdapter.addAll(data);
     }
 
+    //When the loader is reset
     @Override
     public void onLoaderReset(Loader<List<Follower>> loader) {
-        test.setText("Reset");
+
+        mFollowerAdapter.clear();
     }
 }
